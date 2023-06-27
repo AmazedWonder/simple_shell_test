2badcba9a9dd6e61677c769daf0c2238d8c40706
Wonder <giftedwonda@gmail.com>
2023-06-26 19:04:34 -0700

    Write a beautiful code that passes the Betty checks

--- fork.c
+++ fork.c
@@ -5,23 +5,23 @@
  * main - entry point
  * multiplies numbers
  *
- * Return: 0 always 
+ * Return: 0 always
  */
 int main(void)
 {
-  pid_t pid;
+	pid_t pid;
 
-  printf("Before fork, pid is %u\n", pid);
+	printf("Before fork, pid is %u\n", pid);
 
-  fork();
+	fork();
 
-  if (pid == -1)
-  {
-    perror("Error\n");
-    return (1);
-  }
+	if (pid == -1)
+	{
+		perror("Error\n");
+		return (1);
+	}
 
-  printf("after fork, pid is %u\n", pid);
+	printf("after fork, pid is %u\n", pid);
 
-  return (0);
+	return (0);
 }
